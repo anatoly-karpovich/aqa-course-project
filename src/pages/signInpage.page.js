c9a2321ce302a1ce8072ec7234022fd50bce9b3d
Anatoly Karpovich <anatoly.karpovich90@gmail.com>
2025-06-22 04:52:30 +0200

    fixed background color on login page

--- src/pages/signInpage.page.js
+++ src/pages/signInpage.page.js
@@ -73,7 +73,7 @@ const layout = `
   </div>
 </div>
 <div
-  class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5">
+  class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-body">
   <!-- Copyright -->
   <div class="text-body mb-3 mb-md-0">
     Copyright © ${moment().year()}. All rights reserved.
